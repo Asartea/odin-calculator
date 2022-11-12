c8fc87ccaa1865b91f20709655ec1acb77fb1646
Asartea <76259120+Asartea@users.noreply.github.com>
2022-11-12 10:16:27 +0100

    Basic calculator logic

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const DEFAULT_DISPLAY = ''
+let operands = [0,0]
+let operators = []
 const multiply = (num1, num2) => num1*num2
 const add = (num1, num2) => num1 + num2
 const substract = (num1, num2) => num1 - num2
@@ -11,8 +13,8 @@ const operate = (operator, num1, num2) => {
         case '-':
             displayResult(substract(num1, num2))
             break;
-        case '*':
-            displayResult(substract(num1, num2))
+        case 'x':
+            displayResult(multiply(num1, num2))
             break;
         case '/':
             displayResult(divide(num1, num2))
@@ -24,13 +26,45 @@ const operate = (operator, num1, num2) => {
     }
 }
 function setupOperand () {
-    let operand = document.querySelectorAll('.operand')
-    for (let i = 0; i < operand.length; i++)
-        operand[i].addEventListener('onclick', () => {
-            //some code that actually handles calculation
+    let numberButtons = document.querySelectorAll('.operand')
+    numberButtons.forEach(element => {
+        element.addEventListener('click', (e) => {
+            /*some code that actually handles calculation
+                We already know that we're dealing with a operand, and we need to store the value of the first number (which may be an arbitrary size, the second number, and then calculate the result)
+                So we use a array, and check if we have a operator to see if we need to switch arrays
+            */
+           let value = parseInt(e.target.textContent)
+           console.log(value)
+           if (operators.length === 0) {
+            operands[0] += value
+           } else {
+            operands[1] += value
+           }
+           console.log(operands)
         })
+
+    });
+}
+function setupOperator () {
+    let operatorButtons = document.querySelectorAll('.operator')
+    operatorButtons.forEach(element => {
+        element.addEventListener('click', (e) => {
+            let value = e.target.textContent
+            if (operators.length === 0) {
+                operators[0] = value
+                console.log(operators)
+            } else {
+                operate(operators[0], operands[0], operands[1])
+            }
+        })
+    })
 }
 const clear = () => {
     let display = document.querySelector('.display')
     display.textContent = DEFAULT_DISPLAY
 }
+function displayResult(result) {
+    console.log(result)
+}
+setupOperand()
+setupOperator()
