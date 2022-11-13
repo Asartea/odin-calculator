b6b3078b3e1713275d4bcef810bc2632e20c2a97
Asartea <76259120+Asartea@users.noreply.github.com>
2022-11-13 12:08:10 +0100

    Add basic keyboard support

--- script.js
+++ script.js
@@ -35,6 +35,38 @@ function setupOperators() {
         })
     })
 }
+
+// Keyboard handling
+window.addEventListener('keydown', (e) => {
+    if (e.key >= 0 && e.key <= 9) {
+        addNumber(e.key)
+        return
+    }
+    if (e.key === '+' || e.key === '-' || e.key === '/' || e.key === 'x') {
+        addOperation(e.key)
+        return
+    }
+    if (e.key === '.') {
+        addPoint()
+        return
+    }
+    if (e.key === '=' || e.key === 'Enter') {
+        evaluate()
+        return
+    }
+    if (e.key === 'Backspace') {
+        deleteLast()
+        return
+    }
+    if (e.key === 'Delete' || e.key === 'Esc') {
+        reset()
+        return
+    }
+    if (e.key === '*') {
+        addOperation('x')
+    }
+    e.preventDefault()
+})
 setupNumbers()
 setupOperators()
 /* Dom manipulation functions */
